8c56916df4ccee7c49b293fd1c122b01bfcb262e
carissa morrow <carissamorrow@hotmail.com>
2019-02-13 11:13:34 -0700

    building more views

---
+++ src/views/Auth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="auth">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'auth',
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {}
+  }
+
+</script>
+
+<style>
+
+
+</style>
---
+++ src/views/UserDash.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="userdash">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'userdash',
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {}
+  }
+
+</script>
+
+<style>
+
+
+</style>
