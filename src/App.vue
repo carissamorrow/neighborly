077dc5a21a748526b5ca1c28830485a282c1edbe
carissa morrow <carissamorrow@hotmail.com>
2019-02-15 09:06:35 -0700

    building out authentication

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,53 @@
 <template>
   <div id="app">
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <a class="navbar-brand" href="#">Neighborly</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#/">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/userDash">UserDash</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#/login" v-if="!user.id">Login/Register</a>
+            <a class="nav-link" @click="logout" href="#/login" v-else>Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
+
+<script>
+  export default {
+    name: "app",
+    data() {
+      return {
+      }
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
+      }
+    },
+    methods: {
+      logout() {
+        this.$store.dispatch("logout")
+      },
+      login() {
+        this.$store.dispatch("login")
+      },
+    }
+  }
+
+</script>
 
 <style>
   #app {
--- src/store.js
+++ src/store.js
@@ -20,12 +20,55 @@ let api = Axios.create({
 
 export default new Vuex.Store({
   state: {
-
+    user: {},
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user
+    },
+    logout(state) {
+      state.user = {}
+    },
   },
   actions: {
-
+    register({ commit, dispatch }, newUser) {
+      auth.post('register', newUser)
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('[registration failed] :', e)
+        })
+    },
+    authenticate({ commit, dispatch }) {
+      auth.get('authenticate')
+        .then(res => {
+          commit('setUser', res.data)
+          dispatch('getAllVaults')
+          // router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('not authenticated')
+        })
+    },
+    login({ commit, dispatch }, creds) {
+      auth.post('login', creds)
+        .then(res => {
+          commit('setUser', res.data)
+          dispatch('getAllVaults')
+          router.push({ name: 'home' })
+        })
+        .catch(e => {
+          console.log('Login Failed')
+        })
+    },
+    logout({ commit, dispatch }) {
+      auth.delete('/logout')
+        .then(res => {
+          commit('setUser', {})
+          // router.push({ name: 'home' })
+        })
+    }
   }
 })
