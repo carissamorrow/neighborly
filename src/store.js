a4ab9522121d065f1377ca209749d45176863d14
carissa morrow <carissamorrow@hotmail.com>
2019-02-15 08:53:33 -0700

    adding to store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from './router'
+// @ts-ignore
+import Axios from 'axios'
+// @ts-ignore
 
 Vue.use(Vuex)
+
+
+
+let auth = Axios.create({
+  timeout: 6000,
+  withCredentials: true
+})
+let api = Axios.create({
+  timeout: 6000,
+  withCredentials: true
+})
 
 export default new Vuex.Store({
   state: {
