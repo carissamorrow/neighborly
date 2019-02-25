45a2aea65caaaeaca529681154ca7227c5ddcb91
carissa morrow <carissamorrow@hotmail.com>
2019-02-25 16:02:32 -0700

    adding image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home container-fluid-sm">
     <h1>Welcome to Neighborly</h1>
+    <img src="https://wp.zillowstatic.com/trulia/wp-content/uploads/sites/1/2017/04/Home-Prices-HERO.jpg">
   </div>
 </template>
-
 <script>
   // @ is an alias to /src
 
