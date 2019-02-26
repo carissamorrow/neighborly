d0fc281d203b3d9263ed991b14db4f613bd463d2
carissa morrow <carissamorrow@hotmail.com>
2019-02-26 15:01:02 -0700

    building userDash

--- src/views/UserDash.vue
+++ src/views/UserDash.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="userdash container-fluid-sm">
     <h1 class="bottom">Welcome to your Dashboard</h1>
+    <div class="row">
+      <div class="col-12 mt-5">
+        <h3>My Items I've Posted</h3>
+      </div>
+      <div class="col-12 mt-5">
+        <h3>Items I've Borrowed</h3>
+      </div>
+    </div>
   </div>
 </template>
 
